d1aedb40830103b0ffd17f91e336c7366223b124
Shawn Anastasio <shawnanastasio@yahoo.com>
2016-07-12 21:38:12 -0500

    Update BasilC specification compliance level
    Interpreter is now compliant with latest BasilC specifications,
    specifically regarding variable support.

--- main.c
+++ main.c
@@ -26,6 +26,10 @@
 stack_node_t *root;
 stack_node_t *current_stack;
 
+variable_stack_node_t *root_var;
+// Must always point to latest empty node
+variable_stack_node_t *current_var_stack;
+
 bool in_block;
 
 int32_t main(int32_t argc, char **argv) {
@@ -42,6 +46,10 @@ int32_t main(int32_t argc, char **argv) {
     root = (stack_node_t *) malloc(sizeof(stack_node_t));
     current_stack = root;
     stack_node_initialize(root);
+
+    // Create initial variable stack
+    root_var = (variable_stack_node_t *) malloc(sizeof(variable_stack_node_t));
+    current_var_stack = root_var;
 
     // Open script file
     FILE *fp;
@@ -295,6 +303,45 @@ void parse_line(char *line, int32_t line_len, int32_t linenum) {
         goto advance_stack;
     }
 
+    // Check define()
+    if (line_len >= 14 && strncmp(line, "BasilC-define(", 14) == 0) {
+        if (line[line_len-1] != ')') {
+            goto parse_fail;
+        }
+
+        // Add to stack
+        current_stack->command = "define";
+        // If size and parameters are correct
+        if (line_len > 10+1 && str_index_of(line, ",") > -1) {
+            int32_t comma = str_index_of(line, ",");
+            //printf("line_len: %d\ncomma: %d\n", line_len, comma);
+            strncpy(current_stack->parameters[0], line+14, comma-14);
+            strncpy(current_stack->parameters[1], line+comma+2, line_len-comma-3);
+            goto advance_stack;
+        } else {
+            goto parse_fail;
+        }
+
+        goto advance_stack;
+    }
+
+    // Check sayvar()
+    if (line_len >= 14 && strncmp(line, "BasilC-sayvar(", 14) == 0) { // Make sure arguments are closed
+        if (line[line_len-1] != ')') {
+            goto parse_fail;
+        }
+
+        // Add to stack
+        current_stack->command = "sayvar";
+        // If parameter is specified, add it
+        if (line_len > 15) {
+            strncpy(current_stack->parameters[0], line+14, line_len-15);
+        } else {
+            goto parse_fail;
+        }
+        goto advance_stack;
+    }
+
 parse_fail:
     fprintf(stderr, "Invalid command\n");
     fprintf(stderr, "At line %d: %s\n", linenum, line);
@@ -329,6 +376,14 @@ void stack_execute() {
         // say()
         if (strcmp(cur->command, "say") == 0) {
             char *temp = cur->parameters[0];
+            // Try to parse variables in string
+            char *parsed = parse_var_string(temp);
+            if (parsed != NULL) {
+                // If variables were parsed successfully
+                printf("%s\n", parsed);
+                free(parsed);
+                goto loop_next;
+            }
             printf("%s\n", temp);
             goto loop_next;
         }
@@ -396,6 +451,34 @@ void stack_execute() {
             goto loop_next;
         }
 
+        // define()
+        if (strcmp(cur->command, "define") == 0) {
+            char *var_name = cur->parameters[0];
+            char *var_data = cur->parameters[1];
+
+            // Add to variable stack
+            strcpy(current_var_stack->name, var_name);
+            strcpy(current_var_stack->data, var_data);
+
+            // Advance variable stack
+            current_var_stack->next = (variable_stack_node_t *)
+                                      malloc(sizeof(variable_stack_node_t));
+            current_var_stack = current_var_stack->next;
+
+            goto loop_next;
+        }
+
+        // sayvar()
+        if (strcmp(cur->command, "sayvar") == 0) {
+            char *temp = get_data_for_var(cur->parameters[0]);
+            if (temp != NULL) {
+                printf("%s\n", temp);
+                goto loop_next;
+            }
+            printf("null\n");
+            goto loop_next;
+        }
+
     loop_next:
         cur = cur->next;
 
@@ -439,11 +522,11 @@ void set_block_execute(stack_node_t *cur, bool val) {
 bool eval_conditional(char *cond) {
     // Determine sign
     char sign = '\0';
-    if (str_index_of(cond, '=') > -1) {
+    if (str_index_of(cond, "=") > -1) {
         sign = '=';
-    } else if (str_index_of(cond, '>') > -1) {
+    } else if (str_index_of(cond, ">") > -1) {
         sign = '>';
-    } else if (str_index_of(cond, '<') > -1) {
+    } else if (str_index_of(cond, "<") > -1) {
         sign = '<';
     } else {
         exit_with_error("Invalid conditional!");
@@ -452,9 +535,9 @@ bool eval_conditional(char *cond) {
     // Determine parameters
     char param_1[strlen(cond)];
     char param_2[strlen(cond)];
-    if (split_string_delimiter(param_1, cond, ' ') <= -1) {
+    if (split_string_delimiter(param_1, cond, " ") <= -1) {
         exit_with_error("Invalid conditional!");
-    } else if (split_string_delimiter_rev(param_2, cond, ' ') <= -1) {
+    } else if (split_string_delimiter_rev(param_2, cond, " ") <= -1) {
         exit_with_error("Invalid conditional!");
     }
 
@@ -473,10 +556,36 @@ bool eval_conditional(char *cond) {
     }
 }
 
-int32_t str_index_of(char *str, char c) {
-    int32_t i = 0;
+/**
+ * Index of the first occurance of `c` in `str`
+ */
+int32_t str_index_of(char *str, char *c) {
+    int32_t i;
     for (i=0; i<strlen(str); i++) {
-        if (str[i] == c) return i;
+        if (strncmp(str+i, c, strlen(c)) == 0) return i;
+    }
+    return -1;
+}
+
+/**
+ * Index of the `n`th occurance of `c` in `str`
+ */
+int32_t str_index_of_n(char *str, char *c, int32_t n) {
+    int32_t count = 0;
+    int32_t i;
+    for (i=0; i<strlen(str); i++) {
+        if (strncmp(str+i, c, strlen(c)) == 0 && count++ == n) return i;
+    }
+    return -1;
+}
+
+/**
+ * Index of the first occurance of 'c' in 'str' after 'skip'
+ */
+int32_t str_index_of_skip(char *str, char *c, int32_t skip) {
+    int32_t i;
+    for (i=skip; i<strlen(str); i++) {
+        if (strncmp(str+i, c, strlen(c)) == 0) return i;
     }
     return -1;
 }
@@ -489,18 +598,18 @@ void exit_with_error(char *error) {
 /**
  * Returns the all chars in a string up until `delim` into `buf`
  */
-int32_t split_string_delimiter(char *buf, char *str, char delim) {
+int32_t split_string_delimiter(char *buf, char *str, char *delim) {
     int32_t delim_pos = str_index_of(str, delim);
     if (delim_pos <= -1) return -1;
     strncpy(buf, str, delim_pos);
     return 0;
 }
 
-int32_t split_string_delimiter_rev(char *buf, char *str, char delim) {
+int32_t split_string_delimiter_rev(char *buf, char *str, char *delim) {
     int32_t last_delim_pos = -1;
     int32_t i;
     for (i=0; i<strlen(str); i++) {
-        if (str[i] == delim) last_delim_pos = i;
+        if (strncmp(str+i, delim, strlen(delim)) == 0) last_delim_pos = i;
     }
     if (last_delim_pos <= -1) return -1;
 
@@ -508,4 +617,96 @@ int32_t split_string_delimiter_rev(char *buf, char *str, char delim) {
         buf[i] = str[strlen(str)-i-1];
     }
     return 0;
+}
+
+int32_t get_char_occurances(char *str, char *c) {
+    int32_t counter = 0;
+    int32_t i;
+    for (i=0; i<strlen(str); i++) {
+        if (strncmp(str+i, c, strlen(c)) == 0) counter++;
+    }
+    return counter;
+}
+
+char * get_data_for_var(char *var_name) {
+    variable_stack_node_t *cur = root_var;
+    while (cur != NULL) {
+        if (strcmp(cur->name, var_name) == 0) {
+            return cur->name;
+        }
+        cur = cur->next;
+    }
+    return NULL;
+}
+
+void shift_string_left(char *str, int32_t start, int32_t n) {
+    int32_t i, j;
+    // Shift entire string left by one space 'n' times
+    for (i=0; i<n; i++) {
+        // Shift entire string left by one space
+        for (j=start; j<strlen(str); j++) {
+            str[j] = str[j+1];
+        }
+    }
+}
+
+/**
+ * Translates all variables prefixed with $ to "%s" to be parsed later on with
+ * sprintf.
+ */
+void prepare_var_string(char *str, int32_t num_vars) {
+    int32_t i;
+    for (i=0; i<num_vars; i++) {
+        int32_t cur_var_index = str_index_of(str, "$");
+        int32_t cur_var_end_index = str_index_of_skip(str, " ", cur_var_index);
+        if (cur_var_end_index <= -1) cur_var_end_index = strlen(str);
+        int32_t cur_var_length = cur_var_end_index - cur_var_index;
+        shift_string_left(str, cur_var_index, cur_var_length-2); // 2 for %s
+        str[cur_var_index] = '%';
+        str[cur_var_index+1] = 's';
+    }
+}
+
+/**
+ * Parses a string containing variables (prefixed with $) into its actual value
+ * Returns a full string with variables parsed (must be freed after use)
+ * Returns NULL if string contains nonexistent variables
+ */
+char * parse_var_string(char *str) {
+    // Get number of variables
+    int32_t num_vars = get_char_occurances(str, "$");
+
+    // Determine size of buffer
+    int32_t bufsize = strlen(str) + (MAX_DATA_SIZE * num_vars) - num_vars;
+    char *buf = malloc(bufsize);
+
+    // Array of var values in order
+    char var_values[num_vars][MAX_DATA_SIZE];
+    int32_t var_values_size = 0;
+
+    int32_t i;
+    for (i=0; i<num_vars; i++) {
+        int32_t cur_var_index = str_index_of_n(str, "$", i);
+        int32_t cur_var_end_index = str_index_of_skip(str, " ", cur_var_index);
+        if (cur_var_end_index <= -1) cur_var_end_index = strlen(str);
+        int32_t cur_var_length = cur_var_end_index - cur_var_index;
+        char cur_var[MAX_DATA_SIZE];
+        memset(cur_var, '\0', MAX_DATA_SIZE);
+        strncpy(cur_var, str+cur_var_index+1, cur_var_length-1);
+        // Get data for this var
+        char* cur_var_data = get_data_for_var(cur_var);
+        if (cur_var_data == NULL) return NULL;
+
+        // Store in Array
+        strcpy(var_values[var_values_size++], cur_var_data);
+    }
+
+    // Get sprintf format string
+    char formatstr[strlen(str)+1];
+    strcpy(formatstr, str);
+    prepare_var_string(formatstr, num_vars);
+
+    // Get final parsed string
+    sprintf(buf, formatstr, var_values);
+    return buf;
 }
--- main.h
+++ main.h
@@ -5,6 +5,8 @@
 #define STACK_PARAMETER_MAX_AMOUNT 5 // Max parameters a command can have
 #define STACK_PARAMETER_MAX_LENGTH 100 // Max length of a parameter
 
+#define MAX_DATA_SIZE 32
+
 // General stack node contains command and pointer to parameter linked list
 struct stack_node {
     char *command;
@@ -13,16 +15,30 @@ struct stack_node {
     struct stack_node *next;
 };
 
+struct variable_stack_node {
+    char name[MAX_DATA_SIZE];
+    char data[MAX_DATA_SIZE];
+    struct variable_stack_node *next;
+};
+
 typedef struct stack_node stack_node_t;
+typedef struct variable_stack_node variable_stack_node_t;
 
 void stack_node_initialize(struct stack_node *s);
 void parse_line(char *line, int line_len, int linenum);
 void stack_execute();
+void parse_cleanup();
 stack_node_t * stack_search_label(char *label);
 void set_block_execute(stack_node_t *start, bool val);
 bool eval_conditional(char *cond);
-int str_index_of(char *str, char c);
+int32_t str_index_of(char *str, char *c);
+int32_t str_index_of_n(char *str, char *c, int32_t n);
+int32_t str_index_of_skip(char *str, char *c, int32_t skip);
 void exit_with_error(char *error);
-int32_t split_string_delimiter(char *buf, char *str, char delim);
-int32_t split_string_delimiter_rev(char *buf, char *str, char delim);
-void parse_cleanup();
+int32_t split_string_delimiter(char *buf, char *str, char *delim);
+int32_t split_string_delimiter_rev(char *buf, char *str, char *delim);
+int32_t get_char_occurances(char *str, char *c);
+char * get_data_for_var(char *var_name);
+void shift_string_left(char *str, int32_t start, int32_t n);
+void prepare_var_string(char *str, int32_t num_vars);
+char * parse_var_string(char *str);
